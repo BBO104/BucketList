29563a10b59b9086d2091ef79d4d2092a617aa60
BBO104 <babenkoqwertyuiop@gmail.com>
2023-11-24 10:50:14 +0200

    1

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,20 +5,77 @@
 //  Created by mac on 24.11.2023.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var viewModel = ViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        if viewModel.unLocked{
+            ZStack{
+                Map(coordinateRegion: $viewModel.mapRegion, annotationItems: viewModel.locations) { location in
+                    MapAnnotation(coordinate: location.coordinate ){
+                        VStack{
+                            Image(systemName: "star.circle")
+                                .resizable()
+                                .foregroundColor(.red)
+                                .frame(width:44, height: 44)
+                                .background(.secondary)
+                                .clipShape(Circle())
+                            
+                            Text(location.name)
+                                .fixedSize()
+                        }
+                        .onTapGesture {
+                            viewModel.selectedPlace = location
+                        }
+                    }
+                }
+                .ignoresSafeArea()
+                Circle()
+                    .fill(.blue)
+                    .opacity(0.3)
+                    .frame(width: 32)
+                
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Button {
+                            viewModel.addLocation()
+                        } label: {
+                            Image(systemName: "plus")
+                        }
+                        .padding()
+                        .background(.black.opacity(0.75))
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .clipShape(Circle())
+                        .padding(.trailing)
+                    }
+                }
+                .sheet(item: $viewModel.selectedPlace){place in
+                    EditView(location: place){ newLocation in
+                        viewModel.update(location: newLocation)
+                    }
+                    .presentationDetents([.medium])
+                }
+            }
+        } else {
+            Button ("unlock") {
+                viewModel.authenticate()
+            }
+            .padding()
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Capsule())
         }
-        .padding()
     }
-}
-
-#Preview {
-    ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
 }
