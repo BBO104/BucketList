9aeb7dea8dbf8e9e99a78372ebd7b1d37fa25a4d
BBO104 <babenkoqwertyuiop@gmail.com>
2023-11-24 10:22:29 +0200

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by mac on 24.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
