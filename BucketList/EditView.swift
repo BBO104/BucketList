0d6edce0dd572e20cb4d791847f35ab7083a9b46
BBO104 <babenkoqwertyuiop@gmail.com>
2023-12-11 18:12:10 +0200

    2

---
+++ BucketList/EditView.swift
@@ -0,0 +1,18 @@
+//
+//  EditView.swift
+//  BucketList
+//
+//  Created by mac on 11.12.2023.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditView()
+}
---
+++ BucketList/Result.swift
@@ -0,0 +1,8 @@
+//
+//  Result.swift
+//  BucketList
+//
+//  Created by mac on 11.12.2023.
+//
+
+import Foundation
